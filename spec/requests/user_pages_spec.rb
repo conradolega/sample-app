f0413150d2aa66ce9aa0e3bbac452b13f59bd9b3
conradolega <conradolega@gmail.com>
2014-04-09 22:43:02 +0800

    Add after submission test for invalid signup

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -25,7 +25,14 @@ describe "User pages" do
 
     describe "with invalid information" do
       it "should not create a user" do
-        expect { click_button "Create my account" }.not_to change(User, :count)
+        expect { click_button submit }.not_to change(User, :count)
+      end
+
+      describe "after submission" do
+        before { click_button submit }
+
+        it { should have_title('Sign up') }
+        it { should have_content('error') }
       end
     end
 
