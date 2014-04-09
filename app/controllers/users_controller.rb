ee8ecc732a70f0fd018ea27733adb3347fb6aff7
conradolega <conradolega@gmail.com>
2014-04-09 22:09:13 +0800

    Add create method that handles invalid signup in UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,14 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # Handle a successful save.
+    else
+      render 'new'
+    end
+  end
 
   private
 
