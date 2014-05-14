e348371578328645f54c0044f1f25d0719b977ec
conradolega <conradolega@gmail.com>
2014-05-14 23:42:09 +0800

    Add a before filter for the correct user
    The correct_user filter calls the correct_user? function in
    app/helpers/sessions_helper.rb.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   before_action :signed_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+
   def new
     @user = User.new
   end
@@ -40,5 +42,10 @@ class UsersController < ApplicationController
 
     def signed_in_user
       redirect_to signin_url, notice: "Please sign in." unless signed_in?
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to root_url unless current_user?(@user)
     end
 end
